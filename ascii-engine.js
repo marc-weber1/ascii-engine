028422c0184f677fb8f19c27e1e04bd7aec8a7ef
coredata-mweber <marc.weber@coredata.ca>
2024-05-01 03:59:10 -0600

    Add a cube that spins :3

--- ascii-engine.js
+++ ascii-engine.js
@@ -81,12 +81,150 @@ window.onload = function() {
 }
 
 
-function init(gl){
+function degToRad(d) {
+    return d * Math.PI / 180;
+}
 
+var fieldOfViewRadians = degToRad(60);
+var modelXRotationRadians = degToRad(0);
+var modelYRotationRadians = degToRad(0);
+
+var program, positionBuffer, positionLocation, matrixLocation;
+
+// Get the starting time.
+var then = 0;
+
+function init(gl){
+    // setup GLSL program
+    program = webglUtils.createProgramFromScripts(gl, ["vertex-shader", "b-shader"]);
+
+    // look up where the vertex data needs to go.
+    positionLocation = gl.getAttribLocation(program, "a_position");
+
+    // lookup uniforms
+    matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    // Create a buffer for positions
+    positionBuffer = gl.createBuffer();
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Put the positions in the buffer
+    setGeometry(gl);
 }
 
 function draw(gl, time){
-    // Test by drawing lowercase a
+    // convert to seconds
+    time *= 0.001;
+    // Subtract the previous time from the current time
+    var deltaTime = time - then;
+    // Remember the current time for the next frame.
+    then = time;
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, width, height);
+
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Animate the rotation
+    modelYRotationRadians += -0.7 * deltaTime;
+    modelXRotationRadians += -0.4 * deltaTime;
+
+    // Clear the canvas AND the depth buffer.
     gl.clearColor(0.38, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 3;          // 3 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // Compute the projection matrix
+    var aspect = width / height;
+    var projectionMatrix =
+        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+    var cameraPosition = [0, 0, 2];
+    var up = [0, 1, 0];
+    var target = [0, 0, 0];
+
+    // Compute the camera's matrix using look at.
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    var matrix = m4.xRotate(viewProjectionMatrix, modelXRotationRadians);
+    matrix = m4.yRotate(matrix, modelYRotationRadians);
+
+    // Set the matrix.
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+    // Draw the geometry.
+    gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);
 }
+
+// Fill the buffer with the values that define a cube.
+function setGeometry(gl) {
+    var positions = new Float32Array(
+      [
+      -0.5, -0.5,  -0.5,
+      -0.5,  0.5,  -0.5,
+       0.5, -0.5,  -0.5,
+      -0.5,  0.5,  -0.5,
+       0.5,  0.5,  -0.5,
+       0.5, -0.5,  -0.5,
+  
+      -0.5, -0.5,   0.5,
+       0.5, -0.5,   0.5,
+      -0.5,  0.5,   0.5,
+      -0.5,  0.5,   0.5,
+       0.5, -0.5,   0.5,
+       0.5,  0.5,   0.5,
+  
+      -0.5,   0.5, -0.5,
+      -0.5,   0.5,  0.5,
+       0.5,   0.5, -0.5,
+      -0.5,   0.5,  0.5,
+       0.5,   0.5,  0.5,
+       0.5,   0.5, -0.5,
+  
+      -0.5,  -0.5, -0.5,
+       0.5,  -0.5, -0.5,
+      -0.5,  -0.5,  0.5,
+      -0.5,  -0.5,  0.5,
+       0.5,  -0.5, -0.5,
+       0.5,  -0.5,  0.5,
+  
+      -0.5,  -0.5, -0.5,
+      -0.5,  -0.5,  0.5,
+      -0.5,   0.5, -0.5,
+      -0.5,  -0.5,  0.5,
+      -0.5,   0.5,  0.5,
+      -0.5,   0.5, -0.5,
+  
+       0.5,  -0.5, -0.5,
+       0.5,   0.5, -0.5,
+       0.5,  -0.5,  0.5,
+       0.5,  -0.5,  0.5,
+       0.5,   0.5, -0.5,
+       0.5,   0.5,  0.5,
+  
+      ]);
+    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+  }
