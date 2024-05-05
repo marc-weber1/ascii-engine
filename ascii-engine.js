9ed5ad27fcfbc3cbbe50d26e95ee3956019ba3be
Marc <marc.weber54@gmail.com>
2024-05-04 22:20:26 -0600

    Refactor and attempt to implement an arbitrary material brightness map patch:

--- ascii-engine.js
+++ ascii-engine.js
@@ -1,71 +1,41 @@
 import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js'
 
-const width = 80;
-const height = 35;
+const decoder = new TextDecoder();
 
-window.onload = function() {
-    const decoder = new TextDecoder();
-    const ascii_canvas = document.getElementById("ascii-canvas");
+function renderAsText(renderer, htmlElement, width, height){
+    const glContext = renderer.getContext();
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
+    let pixels = new Uint8Array(width * height * 4);
+    glContext.readPixels(0, 0, width, height, glContext.RGBA, glContext.UNSIGNED_BYTE, pixels);
 
-    const gl = renderer.getContext();
-    
-    init(renderer);
-
-    function DrawScene(time)
-    {
-        draw(renderer, time);
-
-        let pixels = new Uint8Array(width * height * 4);
-        gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
-    
-        // Convert e.g. 0x60 0x00 0x00 0x00 into just 0x60
-        let pixels_depadded = new Uint8Array(width * height * 4 + height); // + height for newlines
-        let write_index = 0;
-        for(let p = 0; p < width * height; p++){
-            if(p != 0 && p % width == 0){
-                pixels_depadded[write_index] = 0xA; write_index++; //newline
-            }
-
-            pixels_depadded[write_index] = pixels[4*p]; write_index++;
-            if(pixels[4*p+1] == 0x0) continue;
-            pixels_depadded[write_index] = pixels[4*p+1]; write_index++;
-            if(pixels[4*p+2] == 0x0) continue;
-            pixels_depadded[write_index] = pixels[4*p+2]; write_index++;
+    // Convert e.g. 0x60 0x00 0x00 into just 0x60 (alpha ignored)
+    let pixels_depadded = new Uint8Array(width * height * 4 + height); // + height for newlines
+    let write_index = 0;
+    for(let p = 0; p < width * height; p++){
+        if(p != 0 && p % width == 0){
+            pixels_depadded[write_index] = 0xA; write_index++; //newline
         }
 
-        pixels_depadded = pixels_depadded.slice(0, write_index);
-
-        ascii_canvas.innerText = decoder.decode(pixels_depadded);
-    
-        requestAnimationFrame(DrawScene);
+        pixels_depadded[write_index] = pixels[4*p]; write_index++;
+        if(pixels[4*p+1] == 0x0) continue;
+        pixels_depadded[write_index] = pixels[4*p+1]; write_index++;
+        if(pixels[4*p+2] == 0x0) continue;
+        pixels_depadded[write_index] = pixels[4*p+2]; write_index++;
     }
 
-    requestAnimationFrame(DrawScene);
+    pixels_depadded = pixels_depadded.slice(0, write_index);
+
+    htmlElement.innerText = decoder.decode(pixels_depadded);
 }
 
-
-let scene, camera, cube;
-
-function init(renderer){
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x610000 } );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 2;
-
-    renderer.setClearColor(0x630000);
+function characterTexture(text){ // -> three.js Texture
+    const texture = new THREE.DataTexture({
+        data: new Uint8Array([0x61, 0x00, 0x00, 0x00]),
+        width: 1,
+        height: 1
+    });
+    texture.needsUpdate = true;
+    return texture;
 }
 
-function draw(renderer, time){
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-}
+export { renderAsText, characterTexture }
---
+++ example-scene.js
@@ -0,0 +1,67 @@
+import * as THREE from 'https://unpkg.com/three@0.122.0/build/three.module.js'
+import * as ascii from './ascii-engine.js';
+
+const width = 80;
+const height = 35;
+
+window.onload = function() {
+    const ascii_canvas = document.getElementById("ascii-canvas");
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    
+    init(renderer);
+
+    function DrawScene(time)
+    {
+        draw(renderer, time);
+        ascii.renderAsText(renderer, ascii_canvas, width, height);
+    
+        requestAnimationFrame(DrawScene);
+    }
+
+    requestAnimationFrame(DrawScene);
+}
+
+
+let scene, camera, cube;
+
+function init(renderer){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshPhongMaterial();
+
+    material.userData.brightnessTextMap = { type: "t", value: ascii.characterTexture(document.getElementById('brightness-texture').innerText) };
+
+    material.onBeforeCompile = function(shader){
+        shader.uniforms.brightnessTextMap = material.userData.brightnessTextMap;
+
+        shader.fragmentShader = 'uniform sampler2D brightnessTextMap;\n' + shader.fragmentShader;
+        shader.fragmentShader = shader.fragmentShader.replace(/gl_FragColor = ([^;]*);/, `
+            vec4 final_colour = $1;
+            gl_FragColor = texture(brightnessTextMap, vec2(final_colour.r, 0));
+        `);
+
+        console.log(shader.fragmentShader);
+    }
+
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    const light = new THREE.PointLight( 0xffffff, 1, 100 );
+    light.position.set( 50, 50, 50 );
+    scene.add( light );
+
+    camera.position.z = 2;
+
+    renderer.setClearColor(0x630000);
+}
+
+function draw(renderer, time){
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
